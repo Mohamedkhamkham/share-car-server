b482fabc395af651de33e3b0ce0cf14a6ec8c8ad
Mohamed khamkham <mohamkham1999@gmail.com>
2023-12-03 22:21:46 +0100

    reservas y falta por terminar favoritos

--- routes/favoritos.routes.js
+++ routes/favoritos.routes.js
@@ -1,61 +1,40 @@
-const router = require("express").Router()
-
+const router = require("express").Router();
 const verifyToken = require("../middlewares/verifyToken");
-const Favorito = require('../models/Favorito.model')
+const Favorito = require('../models/Favorito.model');
+const Trip = require('../models/Trip.model');
 
-router.get("/:id", verifyToken, (req, res) => {
-    const { id } = req.params
-    console.log(id)
-    Favorito
-        .find({ user_id: id })
-        .populate('trips')
-        .exec()
-        .then(response =>
-            res.json(response)
-        )
-        .catch(err => next(err));
+// Get user reservations by ID
+router.get("/:id", verifyToken, async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const userFavoritos = await Favorito.findOne({ user_id: id }).populate('trips').exec();
+        res.json(userFavoritos);
+    } catch (err) {
+        next(err);
+    }
 });
 
+// Handle reservation creation and cancellation
+router.post("/", verifyToken, async (req, res, next) => {
+    try {
+        const { user_id, trip_id } = req.body;
+        const userFavoritos = await Favorito.findOne({ user_id });
 
-router.post("/", verifyToken, (req, res, next) => {
-
-    const { user_id } = req.body
-    const { trip_id } = req.body
-
-    let userReservafind = null;
-
-    Favorito
-        .findOne({ user_id: user_id })
-        .then(data => {
-            if (data != null) {
-                if (data.trips.includes(trip_id)) {
-                    Favorito
-                        .updateOne({ user_id, $pull: { trips: trip_id } })
-                        .then(() => res.sendStatus(204))
-                        .catch(err => next(err))
-                } else {
-                    Favorito
-                        .updateOne({ user_id, $push: { trips: trip_id } })
-                        .then(() => res.sendStatus(204))
-                        .catch(err => next(err))
-                }
+        if (userFavoritos) {
+            if (userFavoritos.trips.includes(trip_id)) {
+                await Favorito.updateOne({ user_id, $pull: { trips: trip_id } });
             } else {
-                const document = {
-                    "user_id": user_id,
-                    "trips": [trip_id]
-                }
-
-                Favorito
-                    .create(document)
-                    .then(() => res.sendStatus(204))
-                    .catch(err => next(err))
+                await Favorito.updateOne({ user_id, $push: { trips: trip_id } });
             }
-        })
-        .catch(err => {
-            console.error('Error en la consulta:', err);
-        });
+            res.json(userFavoritos);
+        } else {
+            await Favorito.create({ user_id, trips: [trip_id] });
+            res.json(userFavoritos);
+        }
+    } catch (err) {
+        console.error('Error:', err);
+        next(err);
+    }
+});
 
-
-})
-
-module.exports = router
+module.exports = router;

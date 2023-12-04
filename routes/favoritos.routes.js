0c2eb86d17f4f82b0947835cd99d0dd50a68eb47
Mohamed khamkham <mohamkham1999@gmail.com>
2023-12-04 13:04:46 +0100

    funciona los favoritos

--- routes/favoritos.routes.js
+++ routes/favoritos.routes.js
@@ -3,7 +3,6 @@ const verifyToken = require("../middlewares/verifyToken");
 const Favorito = require('../models/Favorito.model');
 const Trip = require('../models/Trip.model');
 
-// Get user reservations by ID
 router.get("/:id", verifyToken, async (req, res, next) => {
     try {
         const { id } = req.params;
@@ -14,7 +13,6 @@ router.get("/:id", verifyToken, async (req, res, next) => {
     }
 });
 
-// Handle reservation creation and cancellation
 router.post("/", verifyToken, async (req, res, next) => {
     try {
         const { user_id, trip_id } = req.body;
@@ -22,17 +20,21 @@ router.post("/", verifyToken, async (req, res, next) => {
 
         if (userFavoritos) {
             if (userFavoritos.trips.includes(trip_id)) {
+
                 await Favorito.updateOne({ user_id, $pull: { trips: trip_id } });
             } else {
+
                 await Favorito.updateOne({ user_id, $push: { trips: trip_id } });
             }
-            res.json(userFavoritos);
+            const userFavoritosUpdate = await Favorito.findOne({ user_id });
+
+            res.json(userFavoritosUpdate);
         } else {
-            await Favorito.create({ user_id, trips: [trip_id] });
-            res.json(userFavoritos);
+
+            const userFavoritoCreate = await Favorito.create({ user_id, trips: [trip_id] });
+            res.json(userFavoritoCreate);
         }
     } catch (err) {
-        console.error('Error:', err);
         next(err);
     }
 });

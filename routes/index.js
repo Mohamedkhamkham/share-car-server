997b6c4b35e8cd545afc5e5aba5b27f086b4441a
Mohamed khamkham <mohamkham1999@gmail.com>
2023-12-04 18:34:51 +0100

    terminar cloudinary y mostrar el map

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,7 @@ module.exports = app => {
 
     const authRoutes = require("./auth.routes")
     app.use("/api/auth", authRoutes)
+
+    const uploadRoutes = require("./upload.routes")
+    app.use("/api/upload", uploadRoutes)
 }
